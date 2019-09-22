5e27a99807a16efeae02df9c97520eab4866d9f4
samseven7 <anonymous@anonymous>
2019-09-22 18:35:48 +0200

    Bip version2 Sys.argv

--- receiver.py
+++ receiver.py
@@ -1,7 +1,13 @@
 import socket
+import sys
 
 s = socket.socket()
-host = input()
+try:
+    host = sys.argv[1]
+except IndexError:
+    print('Enter host IP address :')
+    host = input()
+
 port = 9000
 s.connect((host, port))
 m = s.recv(1024)
