76280eb71295428e29e3bec4001a363a2c19fc3e
samseven7 <anonymous@anonymous>
2019-09-22 19:20:50 +0200

    Bip version3 classReceiver

--- receiver.py
+++ receiver.py
@@ -1,15 +1,19 @@
 import socket
-import sys
 
-s = socket.socket()
-try:
-    host = sys.argv[1]
-except IndexError:
-    print('Enter host IP address :')
-    host = input()
 
-port = 9000
-s.connect((host, port))
-m = s.recv(1024)
-print(m)
-s.close()
+class Receiver:
+    def __init__(self, host="192.168.1.28", port=9000, size=1024):
+        self.port = port
+        self.host = host
+        self.size = size
+
+    def run(self):
+        s = socket.socket()
+        s.connect((self.host, self.port))
+        m = s.recv(self.size)
+        print(m)
+        s.close()
+
+
+r = Receiver(size=128, host="192.168.1.28")
+r.run()
